ec84e04b229ab68330a599922f7f306686b725a1
T. Holmes <tdholmes@gmail.com>
2015-12-11 11:51:24 -0500

    Create rdm_gear.lua

---
+++ rdm_gear.lua
@@ -0,0 +1,19 @@
+functions user_setup ()
+
+sets.precast['Chainspell'] =
+sets.precast['Convert'] = 
+sets.precast['Composure'] =
+sets.precast['Saboteur'] =
+sets.precast['Spontaneity'] =
+sets.precast['Styme'] =
+
+sets.precast.FC = {}
+sets.midcast.ElementalMagic = {}
+sets.midcast.EnfeeblingMagic = {}
+sets.midcast.EnhancingMagic = {}
+sets.midcast.HealingMagic = {}
+sets.midcast['Stoneskin'] = {}
+sets.midcast['Refresh'] = {}
+sets.midcast['Phalanx'] = {}
+
+end
