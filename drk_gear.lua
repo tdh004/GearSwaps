235075f9d4cd49c0341e2ecf5bba7cce6c2e54a9
T. Holmes <tdholmes@gmail.com>
2015-12-12 17:48:08 -0500

    Update drk_gear.lua
    Adding sets:
    
    -- Tractor
    -- MaxHaste
    
    Questioning the need for sets.midcast['Endark'].  Endark only requires Dark Magic Skill, so a specific set is unneeded.

--- drk_gear.lua
+++ drk_gear.lua
@@ -20,46 +20,46 @@ function user_setup()
 	sets.midcast.MagicAcc = {
 		ammo="Sturm's report",body="Corselet",hands="Redan gloves",legs="Eschite cuisses",feet="Heathen's sollerets",ring1="Omega ring",ring2="Balrahn's ring"
 	}
+	sets.midcast.MaxHaste = {
+		head="HeathenFallen's burgeonet",body="Heathen's Cuirass",hands="Heathen's gauntlets",legs="Ares' Flanchard +1",waist="Dynamic belt +1"
+	}
 	sets.midcast.sTP = {
 		ammo="Hagneia stone",head="Phorcys salade",body="Armada hauberk",hands="Ogier's gauntlets",legs="Ker's flanchard",feet="Mikinaak greaves",neck="Lissome necklace",ear1="Bladeborn earring",ear2="Steelflash earring",waist="Goading belt"
 	}
 	sets.midcast.Trust = {
 		head="Heathen's burgonet",body="Emet harness +1",hands="Redan gloves",legs="Eschite cuisses",feet="Heathen's sollerets"
 	}
+	-- DarkMagic +77
 	sets.midcast.DarkMagic = {
 		head="Ignominy burgeonet",body="Demon's harness",hands="Fall. Fin. Gaunt.",legs="Eschite cuisses",neck="Deceiver's Torque",ear1="Dark Earring",back="Merciful Cape",waist="Casso sash"
 	}
 	sets.midcast.ElementalMagic = set_combine(sets.midcast.sTP, {
 			neck="Aesir torque",back="Merciful cape"
-		}
-	)
+		})
 	sets.midcast.EnfeeblingMagic = set_combine(sets.midcast.MagicAcc, {
 			neck="Enfeebling torque", waist="Casso sash"
-		}
-	)
+		})
 	sets.midcast['Stun'] = set_combine(sets.midcast.DarkMagic, {
 			head="Quauhpilli helm",body="Nuevo coselete",hands="Heathen's gauntlets",legs="Blood cuisses",feet="Mikinaak greaves",neck="Orunmila's torque",waist="Dynamic belt +1",ring1="Prolix ring",ring2="Balrahn's Ring",ammo="Sturm's report"
-		}
-	)
-	sets.midcast['Endark'] = {
-		head="Ignominy burgeonet",body="Heathen's Cuirass",hands="Fall. Fin. Gaunt.",legs="Heathen's Flanchard",neck="Deceiver's Torque",ear1="Dark earring",back="Merciful cape",waist="Casso sash"
+		})
+	-- Why do I need this?
+	sets.midcast['Endark'] = {   
+		head="Ignominy burgeonet",body="Demon's harness",hands="Fall. Fin. Gaunt.",legs="Heathen's Flanchard",neck="Deceiver's Torque",ear1="Dark earring",back="Merciful cape",waist="Casso sash"
 		}
 	sets.midcast['Dread Spikes'] = {
 		head="Fallen's burgeonet",body="Heathen's Cuirass",hands="Ares' gauntlets +1",legs="Ares' flanchard +1"
 	}
 	sets.midcast['Absorb-TP'] = set_combine(sets.midcast.DarkMagic, {
 			body="Corselet",hands="Heathen's gauntlets",feet="Heathen's Sollerets",ring1="Omega Ring",ring2="Balrahn's Ring",ammo="Sturm's report"
-		}
-	)
+		})
 	sets.midcast['Drain'] = set_combine(sets.midcast.DarkMagic, {
 			body="Corselet",hands="Fall. Fin. Gaunt.",feet="Heathen's Sollerets",ring1="Omega Ring",ring2="Balrahn's Ring",ammo="Sturm's report"
-		}
-	)
+		})
 	sets.midcast['Aspir'] = sets.midcast['Drain']
+	sets.midcast['Tractor'] = sets.midcast.MaxHaste
 	sets.midcast.Bio = set_combine(sets.midcast.DarkMagic, {
 			head="Phorcys salade",body="Armada hauberk",legs="Phorcys dirs",feet="Heathen's Sollerets",ammo="Hagneia Stone",ring1="Tyrant's ring",ring2="Rajas Ring",ear1="Bladeborn Earring",neck="Lissome necklace"
-		}
-	)
+		})
 	sets.midcast['Utsusemi'] = sets.midcast.MaxHaste
 
 	sets.idle.Town = {
@@ -83,8 +83,7 @@ function user_setup()
 	}
 	sets.defense.Reraise = set_combine(sets.defense.PDT, {
 			head="Twilight Helm",body="Twilight Mail"
-		}
-	)
+		})
 	sets.defense.MDT = {
 		ammo="",
 		head="",
@@ -153,8 +152,7 @@ function user_setup()
 	sets.engaged.Reraise = set_combine(sets.engaged, {
 			head="Twilight Helm",
 			body="Twilight Mail",
-		}
-	)
+		})
 		
 		
 		
