1b17af79c270e8202c9c5ecb29753c430fc0e9d2
T. Holmes <tdholmes@gmail.com>
2015-12-13 18:51:45 -0500

    Update rdm_gear.lua

--- rdm_gear.lua
+++ rdm_gear.lua
@@ -8,20 +8,17 @@ sets.precast['Saboteur'] = {hands="Estoqueur's Gantherots +2"}
 -- sets.precast['Styme'] =
 
 sets.precast.FC = {
-  legs="Blood cuisses",neck="Orunmila's Torque",ring1="Prolix Ring",earring2="Loquacious Earring"
+  legs="Blood cuisses",neck="Orunmila's Torque",ring1="Prolix Ring",ear2="Loquacious Earring"
 }
 sets.midcast.MaxHaste = {
 }
 sets.midcast.INT
 sets.midcast.MND
 sets.midcast.MagicAcc = {
+  ammo="Sturm's report",ring1="Omega ring",ring2="Bahlran's ring"
 }
 sets.midcast['Dark Magic'] = {
-  hands="Blood Finger gauntlets",
-  back="Merciful Cape",
-  waist="Casso sash",
-  neck="Deceiver's Torque",
-  ear1="Dark earring"
+  hands="Blood Finger gauntlets",back="Merciful Cape",waist="Casso sash",neck="Deceiver's Torque",ear1="Dark earring"
 }
 sets.midcast['Elemental Magic'] = {
   neck="Aesir torque",
@@ -32,20 +29,16 @@ sets.midcast['Enfeebling Magic'] = {
   neck="Enfeebling Torque"
 }
 sets.midcast['Enhancing Magic'] = {
-  hands="Estoqueur's Gantherots +2"
-  back="Merciful cape"
+  hands="Estoqueur's Gantherots +2",neck="Enhancing Torque",back="Merciful cape"
 }
-sets.midcast.HealingMagic = {}
-sets.midcast['Stoneskin'] = set_combine(sets.midcast.EnhancingMagic, {
-    hands="Stone Mufflers",
-    legs="Haven Hose",
-    neck="Stone Gorget",
-    waist="Siegel Sash"
+sets.midcast['Healing Magic'] = {}
+sets.midcast['Stoneskin'] = set_combine(sets.midcast['Enhancing Magic'], {
+    hands="Stone Mufflers",legs="Haven Hose",neck="Stone Gorget",waist="Siegel Sash"
   })
 sets.midcast['Refresh'] = set_combine(sets.midcast.MaxHaste, {
   legs="Estoqueur's Fuseau +2"
 })
-sets.midcast['Phalanx'] = set_combine(sets.midcast.EnhancingMagic, {
+sets.midcast['Phalanx'] = set_combine(sets.midcast['Enhancing Magic'], {
     })
 sets.midcast['Stun'] = set_combine(sets.midcast.MaxHaste, {
     neck="Deceiver's torque",ear1="Dark earring",back="Merciful cape",ammo="Sturm's report"
